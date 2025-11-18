5291dc97a6b3e12effc66d557d0531823483bf50
Aomine11 <amixenon@gmail.com>
2025-11-18 09:19:14 +0100

    o

--- src/app/pages/pages/doctors-page/doctors-page.ts
+++ src/app/pages/pages/doctors-page/doctors-page.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DoctorsService } from '../../services/doctors.service';
+import { Doctor } from './types/doctor.interface';
 
 @Component({
-  selector: 'app-doctors-page',
-  imports: [],
-  templateUrl: './doctors-page.html',
-  styleUrl: './doctors-page.css'
+  selector: 'app-doctors-page',  
+  standalone: true,  
+  imports: [CommonModule],  
+  templateUrl: './doctors-page.component.html',  
+  styleUrls: ['./doctors-page.component.scss']
 })
-export class DoctorsPage {
-
+export class DoctorsPageComponent {
+  private doctorsService = inject(DoctorsService);  
+  
+  doctors = toSignal(this.doctorsService.getDoctors(), {
+    initialValue: [] as Doctor[]
+  });
 }
